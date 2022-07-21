f8ea6ae5c1cdafb6061f0eab89867a358591d9af
Emilia Hernandez <emilia.b.hernandez@gmail.com>
2022-07-20 21:15:14 -0400

    Revert "first major update"
    This reverts commit 8b2b159ca0eb93f6bb8e5b7068aea07d15dee1cc.

--- test.js
+++ test.js
@@ -1,12 +1,1 @@
-import { createPopper } from '@popperjs/core';
-import 'bootstrap';
-import 'bootstrap/js/dist/util';
-import 'bootstrap/js/dist/dropdown';
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+$(".dropdown-toggle").dropdown();
