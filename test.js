edf5d8e43ba783d7379902ffc354d8bb922e7acc
Emilia Hernandez <emilia.b.hernandez@gmail.com>
2022-11-03 19:31:55 -0600

    newnew

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-$(".dropdown-toggle").dropdown();
+import { createPopper } from '@popperjs/core';
+import 'bootstrap';
+import 'bootstrap/js/dist/util';
+import 'bootstrap/js/dist/dropdown';
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
