8b43f9cb063fd049036ac7a4d953e93b569d7644
Emilia Hernandez <emilia.b.hernandez@gmail.com>
2022-12-13 11:01:19 -0500

    Updating mediaqueries

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -749,10 +749,6 @@ const elephantsArray = [
 let htmlCode = ``;
 // to get each single elephant object from the array and use them to build out html string, we need to open up our array, and we do that using forEach method. The forEach method, finds each item(object) in the array and returns them, this means we get 4 items that are objects back.
 elephantsArray.forEach(function(singleElephantObjects) {
-    // uncomment the line below, to see each of the 4 objects rendered in the console.
-    //console.log(singleElephantObjects);
-    // we take our previous empty htmlCode variable and add our html codes to it.
-    // because the forEach method returns objects, we can then use the dot notation to reference children of the object, e.g, elephant.name;
     htmlCode = htmlCode + `
     <article>
       <div>
@@ -765,16 +761,8 @@ elephantsArray.forEach(function(singleElephantObjects) {
       </div>
     </article>
   `;
-// uncomment the line below to see the output in the browser console.
-// console.log(htmlCode);
 });
-// currently, we have out html code showing in the browser console, we need to send it to our html file and render it there.
-// here we define out the cards we want to be rendered to the DOM.
-// ".all-elephant-cards" is the class of the empty div back in our index.html. We want to render the cards from our JS inside that div.
-// we are simply saying, "let elephantCards be = to that div", to target that div, we reference the class we gave to it.
 const elephantCards = document.querySelector(".all-elephant-cards");
-// here's how we do the render;
-// since elephantCards is now = to that div, we now say let the inside of that div take in our htmlCode variable that holds our html codes.
 elephantCards.innerHTML = htmlCode;
 
 },{}]},["7Aums","bNKaB"], "bNKaB", "parcelRequire716c")
