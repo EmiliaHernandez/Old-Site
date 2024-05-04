533ee697b53bf9fabec1062764d11e4c1e006f35
Emilia Hernandez <emilia.b.hernandez@gmail.com>
2024-05-04 15:05:50 -0400

    ThankYouNote Update
    Making this page into a template

--- src/app.js
+++ src/app.js
@@ -2,30 +2,30 @@
 const elephantsArray = [
   {
     id: 1,
-    name: "Byleth(Both Versions)",
-    IP: "Fire Emblem",
-    desc: "They are both fine",
+    name: "",
+    IP: "",
+    desc: "",
     pictureUrl:
-      "https://pbs.twimg.com/media/EBoxPMzXUAA3URH.png",
-    imdescAlt: "They are both fine"
+      "",
+    imdescAlt: ""
   },
   {
     id: 2,
-    name: "C3PO",
-    IP: "Star Wars",
-    desc: "gay is okay",
+    name: "",
+    IP: "",
+    desc: "",
     pictureUrl:
-      "https://preview.free3d.com/img/2015/03/2408157595903722762/meftys0n.jpg",
-    imdescAlt: "dark elephant"
+      "",
+    imdescAlt: ""
   },
   {
     id: 3,
-    name: "Cana",
-    IP: "Fairy Tail",
-    desc: "(.)(.)🍷",
+    name: "",
+    IP: "",
+    desc: "",
     pictureUrl:
-      "https://64.media.tumblr.com/05c0d65f0ca79483eeea2d6e2eeb9c6e/tumblr_inline_o2wgyqHcRz1t4w1ob_400.png",
-    imdescAlt: "papa elephant"
+      "",
+    imdescAlt: ""
   },
 ];
 
