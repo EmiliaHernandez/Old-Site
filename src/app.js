adb00841492358d0ce746a0b31de5bf0b6b9d9bf
Emilia Hernandez <emilia.b.hernandez@gmail.com>
2024-05-08 22:45:27 -0400

    App.js additional data

--- src/app.js
+++ src/app.js
@@ -18,14 +18,125 @@ const elephantsArraysByUtmTerm = {
   "myles": [
     {
       id: 1,
-      pictureUrl: "ucfPics/Myles1.jpg",
+      pictureUrl: "./ucfPics/Myles1.jpg",
     },
-    // Add more elephants as needed
+    {
+      id: 2,
+      pictureUrl: "./ucfPics/Myles2.jpg",
+    },
+    {
+      id: 3,
+      pictureUrl: "./ucfPics/Myles3.jpg",
+    },
+    {
+      id: 4,
+      pictureUrl: "./ucfPics/Myles4.jpg",
+    },
+    {
+      id: 5,
+      pictureUrl: "./ucfPics/Myles5.jpg",
+    },
+    {
+      id: 6,
+      pictureUrl: "./ucfPics/Myles6.jpg",
+    },
+    {
+      id: 7,
+      pictureUrl: "./ucfPics/bolivar.jpg",
+    },
+    {
+      id: 8,
+      pictureUrl: "./ucfPics/bolivar3.jpg",
+    },
+    {
+      id: 9,
+      pictureUrl: "./ucfPics/multi18.jpg",
+    },
+    {
+      id: 10,
+      pictureUrl: "./ucfPics/multi19.jpg",
+    },
+    {
+      id: 11,
+      pictureUrl: "./ucfPics/multi22.jpg",
+    },
+    {
+      id: 12,
+      pictureUrl: "./ucfPics/multi23.jpg",
+    },
+    {
+      id: 13,
+      pictureUrl: "./ucfPics/multi27.jpg",
+    },
+    {
+      id: 14,
+      pictureUrl: "./ucfPics/multi28.jpg",
+    },
+    {
+      id: 15,
+      pictureUrl: "./ucfPics/multi30.jpg",
+    }
   ],
-  "value2": [
+  "cristina": [
     {
       id: 1,
-      pictureUrl: "URL 2",
+      pictureUrl: "./ucfPics/Cristina1.jpg",
+    },
+    {
+      id: 2,
+      pictureUrl: "./ucfPics/Cristina2.jpg",
+    },
+    {
+      id: 3,
+      pictureUrl: "./ucfPics/Cristina3.jpg",
+    },
+    {
+      id: 4,
+      pictureUrl: "./ucfPics/Cristina4.jpg",
+    },
+    {
+      id: 5,
+      pictureUrl: "./ucfPics/Cristina5.jpg",
+    },
+    {
+      id: 6,
+      pictureUrl: "./ucfPics/Cristina6.jpg",
+    },
+    {
+      id: 7,
+      pictureUrl: "./ucfPics/Cristina7.jpg",
+    },
+    {
+      id: 8,
+      pictureUrl: "./ucfPics/Cristina8.jpg",
+    },
+    {
+      id: 9,
+      pictureUrl: "./ucfPics/Cristina9.jpg",
+    },
+    {
+      id: 10,
+      pictureUrl: "./ucfPics/Cristina10.jpg",
+    },
+    {
+      id: 11,
+      pictureUrl: "./ucfPics/Cristina11.jpg",
+    },
+    {
+      id: 12,
+      pictureUrl: "./ucfPics/Cristina12.jpg",
+    },
+    {
+      id: 13,
+      pictureUrl: "./ucfPics/Cristina13.jpg",
+    },
+    {
+      id: 14,
+      pictureUrl: "./ucfPics/Cristina14.jpg",
+    },
+    {
+      id: 15,
+      pictureUrl: "./ucfPics/Cristina15.jpg",
     },
     // Add more elephants as needed
   ],
